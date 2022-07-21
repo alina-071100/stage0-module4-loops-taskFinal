1d98fc60641cfd990e96a34bbb22e5f46b023dfa
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-07-21 15:12:22 +0400

    Final Task pyramid , power, half pyramid

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,8 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = i; j < cathetusLength-1; j++) {
+                System.out.print(" ");
+                }
+            for (int k = 0; k <=i  ; k++) {
+                System.out.print("*");
 
-    }
+            }
+            System.out.println();
+        }
+        }
+
 
     public static void main(String[] args) {
         new HalfPyramid().printHalfPyramid(8);
